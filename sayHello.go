f43e26885b30c30106f20aa0700ef47cd1cc7c9f
MonaKecil <flowyourminds@gmail.com>
2024-10-18 13:05:43 +0700

    Hello with filter

--- sayHello.go
+++ sayHello.go
@@ -1,7 +1,18 @@
 package SayHello
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Hello(name string) {
 	fmt.Println("Hello " + name)
+}
+
+func HelloFilter(name string) {
+	if strings.Contains(strings.ToLower(name), "anjing") {
+		fmt.Println("Hello ...")
+	} else {
+		fmt.Println("Hello " + name)
+	}
 }
